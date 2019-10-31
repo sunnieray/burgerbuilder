8ee9f464b722bf51d7cb7e9d87dd19864832451c
sunnieray <sunnieray1@gmail.com>
2019-10-31 04:16:49 -0500

    test

---
+++ script.js
@@ -0,0 +1,47 @@
+// let wholeWheat = document.getElementById("wholeWheat").value
+// let honeyOat = document.getElementById("honeyOat").value
+// let seasameSeed = document.getElementById("seasameSeed").value
+// //condiments var
+// let ketchup = document.getElementById("ketchup").value
+// let mayo = document.getElementById("mayo").value
+// let mustard = document.getElementById("mustard").value
+// let spicyMustard = document.getElementById("spicyMustard").value
+// let relish = document.getElementById("relish").value
+// //patty var
+// let beef = document.getElementById("beef").value
+// let turkey = document.getElementById("turkey").value
+// let veggie = document.getElementById("veggie").value
+// let blackBean = document.getElementById("blackBean").value
+// let doubleBeef = document.getElementById("doubleBeef").value
+// //toppings var
+// let lettus = document.getElementById("lettus").value
+// let tomatoes = document.getElementById("tomatoes").value
+// let pickles = document.getElementById("pickles").value
+// let onions = document.getElementById("onions").value
+// let spinach = document.getElementById("spinach").value
+// //sides
+// let fries = document.getElementById("fries").value
+// let chips = document.getElementById("chips").value
+// let carrots = document.getElementById("carrots").value
+// //instructions
+// let instructions = document.getElementById("carrots").value
+
+// //buttons
+// const calculateBtn = document.querySelector('#calculateBtn');
+// const enterBtn = document.querySelector('#enterBtn');
+
+// console.log(wholeWheat)
+
+// // loadEventListeners();
+// // function loadEventListeners() {
+// //   //add a click event listener for the calculate button that triggers a createBudget function
+// //   calculateBtn.addEventListener('click', calculateTotal);
+// //   //add a click event listener for the add expense button that triggers an addExpense function
+// //   enterBtn.addEventListener('click', addInstructions);
+// // }
+
+
+const total = document.getElementById("totalPrice").value
+var sum =
+`${'totalprice'}`
+console.log(sum)
